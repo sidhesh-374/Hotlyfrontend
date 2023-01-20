cc1b2245c4b1ff254136d545dcaeb471c7cd0426
Sidhesh374 <sidheshwar.pandhare@gmail.com>
2023-01-21 00:19:13 +0530

    Update App.js
    Changes are good

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>this code edited on ipad pro with magic keyboard</h2>
     </div>
   );
 }
+
